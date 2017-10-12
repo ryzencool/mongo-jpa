0d68cabfa699b56416a1d878d746a6b36005f788
zmy <zhoumeiyong@qianmi.com>
2017-10-12 16:22:58 +0800

    add: aggregation type

--- src/main/java/com/zmy/springbooy/mongojpa/controller/MottoController.java
+++ src/main/java/com/zmy/springbooy/mongojpa/controller/MottoController.java
@@ -4,12 +4,15 @@ import com.zmy.springbooy.mongojpa.enity.HeroResult;
 import com.zmy.springbooy.mongojpa.enity.HeroTemplate;
 import com.zmy.springbooy.mongojpa.repository.HeroTemplateRepository;
 import com.zmy.springbooy.mongojpa.util.DateUtil;
+import com.zmy.springbooy.mongojpa.util.TestConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +21,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
@@ -25,13 +30,14 @@ import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 public class MottoController {
 
     private final HeroTemplateRepository heroTemplateRepository;
-
+    private final TestConfigurationProperties testConfigurationProperties;
 
     private ReactiveMongoTemplate reactiveMongoTemplate;
 
-    public MottoController(HeroTemplateRepository heroTemplateRepository, ReactiveMongoTemplate reactiveMongoTemplate) {
+    public MottoController(HeroTemplateRepository heroTemplateRepository, ReactiveMongoTemplate reactiveMongoTemplate, TestConfigurationProperties testConfigurationProperties) {
         this.heroTemplateRepository = heroTemplateRepository;
         this.reactiveMongoTemplate = reactiveMongoTemplate;
+        this.testConfigurationProperties = testConfigurationProperties;
     }
 
     @GetMapping("/test")
@@ -60,10 +66,31 @@ public class MottoController {
         Aggregation aggregation = newAggregation(
                 group("heroType").count().as("count").first("heroType").as("name"),
                 project("name", "count"),
-                sort(Sort.Direction.DESC, "count")
+                sort(Sort.Direction.DESC, "count"),
+                match(Criteria.where("count").gt(0))
         );
         Flux<HeroResult> heroResults = reactiveMongoTemplate.aggregate(aggregation, "heroTemplate", HeroResult.class);
         return heroResults;
+    }
+
+
+    /**
+     * 查所有属性种类
+     *
+     * @return
+     */
+    @GetMapping("/getType")
+    public Flux<Map<String, Object>> getType() {
+        Aggregation aggregation = newAggregation(
+                group("heroType").first("heroType").as("type"),
+                project("type")
+        );
+        return reactiveMongoTemplate.aggregate(aggregation, "heroTemplate", HeroResult.class)
+                .map(i -> {
+                    Map<String, Object> tempMap = new HashMap<>();
+                    tempMap.put("type", i.getType());
+                    return tempMap;
+                });
     }
 
     @GetMapping("/queryAll")
@@ -98,16 +125,6 @@ public class MottoController {
         return heroTemplateRepository.countByHeroType(templateType);
     }
 
-//    /**
-//     * 传递一个对象过来
-//     * @return
-//     */
-//    @GetMapping("/queryByTemplate")
-//    public Flux<HeroTemplate> queryByTemplate() {
-//        HeroTemplate heroTemplate = new HeroTemplate();
-//        heroTemplate.setHeroType("士");
-//        return heroTemplateRepository.findAll(Example.of(heroTemplate, ExampleMatcher.matching().withMatcher("createTime", i -> i.contains())) );
-//    }
 
     /**
      * 查找对象
@@ -122,6 +139,12 @@ public class MottoController {
 
     }
 
+    /**
+     * 传递对象的模糊查询
+     *
+     * @param heroTemplate
+     * @return
+     */
     @GetMapping("/queryByTemplateBlur")
     public Flux<HeroTemplate> queryTemplateBlur(@RequestBody HeroTemplate heroTemplate) {
         return heroTemplateRepository.findAll(
@@ -131,5 +154,6 @@ public class MottoController {
                                 .withMatcher("heroName", i -> i.startsWith())));
 
     }
+
 
 }
